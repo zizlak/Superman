aeeaa7818b1ee1c773243663c47bd9fea36f564a
Aleksandr Kurdiukov <runoleonard@gmail.com>
2020-10-02 18:14:29 +0200

    tf

--- FyberProject/ViewController.m
+++ FyberProject/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *iDTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tokenTextField;
+
 @end
 
 @implementation ViewController
